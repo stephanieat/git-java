10ecd4f059c44d2a79325624203db5d049fe6558
Stephanie Thompson <sthompson14@stu.jsu.edu>
2021-01-24 10:13:59 -0600

    Added f3()

--- Class1.java
+++ Class1.java
@@ -5,5 +5,9 @@ public class Class 1 {
 	private void f2() {
 		//Placeholder method (v2.0)
 	}
+	private void f3() {
+		//Placeholder method (v3.0)
+	}
+
 
 }
--- Class2.java
+++ Class2.java
@@ -5,6 +5,10 @@ public class Class 2 {
 	private void f2() {
 		//Placeholder method (v2.0)
 	}
+	private void f3() {
+		//Placeholder method (v3.0)
+	}
+
 }
 	
 }
